ef5acd4a63614da40ccbcf7ff9e7c9b6ea67fd10
Sergey Peskov <sergeypeskov@MacBook-Air-Sergey.local>
2022-01-17 23:28:06 +0300

    prevent rerender on detail page

--- components/detail/templates/detail.tsx
+++ components/detail/templates/detail.tsx
@@ -6,7 +6,7 @@ import {
   View,
   SafeAreaView,
 } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import styles from "./detail.scss";
 
@@ -30,61 +30,71 @@ const Detail = ({ route }: any) => {
     fetch(`https://api.coingecko.com/api/v3/coins/${id}`).then((response) =>
       response.json().then((data) => (setCoinData(data), setLoading(true)))
     );
-  }, []);
+  }, [loading]);
 
-  return loading ? (
-    <SafeAreaView className={styles.container}>
-      <ScrollView>
-        <View className={styles.infoContainer}>
-          <Text className={styles.info}>Name: {coinData?.name}</Text>
+  const getCoinData = useCallback(
+    () =>
+      loading ? (
+        <SafeAreaView className={styles.container}>
+          <ScrollView>
+            <View className={styles.infoContainer}>
+              <Text className={styles.info}>
+                Name: {coinData?.name} {alert("main")}
+              </Text>
+            </View>
+            <View className={styles.infoContainer}>
+              <Text className={styles.info}>Symbol: {coinData?.symbol}</Text>
+            </View>
+            {coinData?.hashing_algorithm && (
+              <View className={styles.infoContainer}>
+                <Text className={styles.info}>
+                  Hashing algorithm: {coinData?.hashing_algorithm}
+                </Text>
+              </View>
+            )}
+            {coinData?.description.en !== "" && (
+              <View className={styles.scrollContainer}>
+                <ScrollView>
+                  <Text className={styles.info}>
+                    {coinData?.description.en}
+                  </Text>
+                </ScrollView>
+              </View>
+            )}
+            <View className={styles.infoContainer}>
+              <Text className={styles.info}>
+                Market cap rating: {coinData?.market_cap_rank}
+              </Text>
+            </View>
+            <View className={styles.infoContainer}>
+              <Text className={styles.info}>
+                Homepage:{" "}
+                <Text
+                  onPress={() => Linking.openURL(coinData?.links.homepage[0])}
+                  className={styles.info}
+                >
+                  {coinData?.links.homepage[0]}
+                </Text>
+              </Text>
+            </View>
+            {coinData?.genesis_date && (
+              <View className={styles.infoContainer}>
+                <Text className={styles.info}>
+                  Genesis date: {coinData?.genesis_date}
+                </Text>
+              </View>
+            )}
+          </ScrollView>
+        </SafeAreaView>
+      ) : (
+        <View className={styles.spinnerContainer}>
+          <ActivityIndicator />
         </View>
-        <View className={styles.infoContainer}>
-          <Text className={styles.info}>Symbol: {coinData?.symbol}</Text>
-        </View>
-        {coinData?.hashing_algorithm && (
-          <View className={styles.infoContainer}>
-            <Text className={styles.info}>
-              Hashing algorithm: {coinData?.hashing_algorithm}
-            </Text>
-          </View>
-        )}
-        {coinData?.description.en !== "" && (
-          <View className={styles.scrollContainer}>
-            <ScrollView>
-              <Text className={styles.info}>{coinData?.description.en}</Text>
-            </ScrollView>
-          </View>
-        )}
-        <View className={styles.infoContainer}>
-          <Text className={styles.info}>
-            Market cap rating: {coinData?.market_cap_rank}
-          </Text>
-        </View>
-        <View className={styles.infoContainer}>
-          <Text className={styles.info}>
-            Homepage:{" "}
-            <Text
-              onPress={() => Linking.openURL(coinData?.links.homepage[0])}
-              className={styles.info}
-            >
-              {coinData?.links.homepage[0]}
-            </Text>
-          </Text>
-        </View>
-        {coinData?.genesis_date && (
-          <View className={styles.infoContainer}>
-            <Text className={styles.info}>
-              Genesis date: {coinData?.genesis_date}
-            </Text>
-          </View>
-        )}
-      </ScrollView>
-    </SafeAreaView>
-  ) : (
-    <View className={styles.spinnerContainer}>
-      <ActivityIndicator />
-    </View>
+      ),
+    [coinData]
   );
+
+  return getCoinData();
 };
 
 export default Detail;
